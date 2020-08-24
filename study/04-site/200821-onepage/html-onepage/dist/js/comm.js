adc91e91661317cbe69f97c55351fdbaa9389f8e
정미애 <jeongmiae@jeongmiaeui-MacBookAir.local>
2020-08-24 13:21:44 +0900

    onepage

--- study/04-site/200821-onepage/html-onepage/dist/js/comm.js
+++ study/04-site/200821-onepage/html-onepage/dist/js/comm.js
@@ -4,6 +4,34 @@ $(document).ready(function () {
   $('.slide-main').slick({
       autoplay: true,
       autoplaySpeed: 3000,
+      arrows: false,
+      dots: true,
+  });
+
+  // slide-festival
+  $('.slide-festival').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [{
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
   });
 
 });
