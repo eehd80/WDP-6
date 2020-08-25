378e714e1603081ec9cff4c1292e751737d6972f
정미애 <jeongmiae@jeongmiaeui-MacBookAir.local>
2020-08-25 13:12:09 +0900

    onepage -scrollUp, scrollTo

--- study/04-site/200821-onepage/html-onepage/dist/js/comm.js
+++ study/04-site/200821-onepage/html-onepage/dist/js/comm.js
@@ -34,8 +34,22 @@ $(document).ready(function () {
     ]
   });
 
-  // $(function () {
-  //   $.scrollUp();
-  // });
+  // scrollUp
+  $.scrollUp({
+    scrollName: 'scrollUp', // Element ID
+    topDistance: '300', // Distance from top before showing element (px)
+    topSpeed: 300, // Speed back to top (ms)
+    animation: 'fade', // Fade, slide, none
+    animationInSpeed: 200, // Animation in speed (ms)
+    animationOutSpeed: 200, // Animation out speed (ms)
+    scrollText: 'Scroll to top', // Text for element
+    activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+  });
+
+  // scrollTo
+  $('nav a').on('click', function (event) {
+    $.scrollTo(this.hash || 0, 500);
+    event.preventDefault();
+  })
 
 });
